a8240109c2be78c0ed0af7867aae8063962144d1
devbab <devbab@pampa.biz>
2022-02-08 10:52:01 +0100

    handle first/always geolocation

--- zoom-center.js
+++ zoom-center.js
@@ -15,35 +15,47 @@ let _prefix, _center, _zoom, _map;
  * @param {string} options.name  default prefix to store localStorage variables 
  * @param {number} options.zoom  default zoom
  * @param {Object} options.center  default center {lat,lng}
- * @param {Boolean} options.geolocation  true if geolocation requested
+ * @param {string} options.geolocation  "always" or "first"
  * 
  */
+
 function init(options) {
+    let firstTime = false;
+
     _prefix = options?.name ? options.name : "";
 
     _zoom = parseInt(localStorage.getItem(`${_prefix}zoom`));
 
     try { _center = JSON.parse(localStorage.getItem(`${_prefix}center`)); }
-    catch (e) {    /* empty */ }
+    catch (e) {
+        firstTime = true; // can't parse, probably nothing in localStorage, so first time
+    }
+
 
     // a bit of basic type/content checking
     if (!_center
         || typeof (_center) !== "object"
         || !Object.prototype.hasOwnProperty.call(_center, "lat")
         || !Object.prototype.hasOwnProperty.call(_center, "lng")
-    )
+    ) {
+        firstTime = true; // can't parse, probably nothing in localStorage, so first time
+
         _center = options.center;
+    }
     if (!_zoom || _zoom <= 0 || typeof (_zoom) !== "number") _zoom = options.zoom;
 
 
     setLocalStorage(_zoom, _center);
 
-
     // if options.geolocation =true, get user locaziton and center map accordingly
     if (options?.geolocation && navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(position => {
-            if (_map) _map.setCenter({ lat: position.coords.latitude, lng: position.coords.longitude });
-        });
+        let geoloc = false;
+        if (options.geolocation == "first" && firstTime) geoloc = true;
+        else if (options.geolocation == "always") geoloc = true;
+        if (geoloc)
+            navigator.geolocation.getCurrentPosition(position => {
+                if (_map) _map.setCenter({ lat: position.coords.latitude, lng: position.coords.longitude });
+            });
     }
 
 
@@ -55,10 +67,13 @@ function zoom() {
 }
 
 function center() {
+    console.log(`get center`, JSON.parse(localStorage.getItem(`${_prefix}center`)));
+
     return JSON.parse(localStorage.getItem(`${_prefix}center`))
 }
 
 function setLocalStorage(zoom, center) {
+    console.log(`set localStorage center`, JSON.stringify(center));
     localStorage.setItem(`${_prefix}zoom`, zoom);
     localStorage.setItem(`${_prefix}center`, JSON.stringify(center));
 }
@@ -71,8 +86,9 @@ function setLocalStorage(zoom, center) {
  */
 function track(map) {
     _map = map;
+
     // map boundaries have changed, store the new center/zoom
-    map.addListener("bounds_changed", () => {
+    map.addListener("idle", () => {
         setLocalStorage(map.getZoom(), map.getCenter().toJSON())
     });
 }
